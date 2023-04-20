815b75050338e6b43238779b5de7f35248982a24
MEDA <118144136+medaworld@users.noreply.github.com>
2023-04-19 20:18:00 -0700

    Add custom mocking logic

--- dealing-side-effects/src/util/io.test.js
+++ dealing-side-effects/src/util/io.test.js
@@ -1,9 +1,18 @@
-import { it, expect, vi } from 'vitest';
+import { it, expect, vi, test } from 'vitest';
 import { promises as fs } from 'fs';
 
 import writeData from './io';
 
 vi.mock('fs');
+vi.mock('path', () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 it('should execute the writeFile method', () => {
   const testData = 'Test';
@@ -11,5 +20,6 @@ it('should execute the writeFile method', () => {
 
   writeData(testData, testFilename);
   // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
-  expect(fs.writeFile).toBeCalled();
+  // expect(fs.writeFile).toBeCalled();
+  expect(fs.writeFile).toBeCalledWith(testFilename, testData);
 });
