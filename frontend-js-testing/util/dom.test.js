72272bccefd0a91e54122e55d81536e7df2a0947
MEDA <118144136+medaworld@users.noreply.github.com>
2023-05-01 23:30:12 -0700

    Add DOM functionality test

--- frontend-js-testing/util/dom.test.js
+++ frontend-js-testing/util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import { it, vi } from 'vitest';
+import { expect, it, vi } from 'vitest';
 import { Window } from 'happy-dom';
 
 import { showError } from './dom';
@@ -14,6 +14,11 @@ const document = window.document;
 document.write(htmlDocumentContent);
 vi.stubGlobal('document', document);
 
-it('first test', () => {
-  showError('test');
+it('should add an error paragraph to the id="errors" element', () => {
+  showError('Test');
+
+  const errorsEl = document.getElementById('errors');
+  const errorParagraph = errorsEl.firstElementChild;
+
+  expect(errorParagraph).not.toBeNull();
 });
